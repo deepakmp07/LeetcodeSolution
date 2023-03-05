dc7908842fbdab0002d3a91badf801d6f7b8b6e0
Deepak Palanisamy <108180358+deepakmp07@users.noreply.github.com>
2023-03-05 21:01:29 +0530

    Delete sql directory

--- sql/1757. Recyclable and Low Fat Products.sql
+++
@@ -1,3 +0,0 @@
-# Write your MySQL query statement below
-SELECT product_id FROM Products
-where low_fats = "Y" AND recyclable = "Y";
--- sql/183. Customers Who Never Order.sql
+++
@@ -1,3 +0,0 @@
-# Write your MySQL query statement below
-SELECT name Customers FROM Customers 
-where id NOT IN(SELECT customerid from Orders);
